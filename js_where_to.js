beea8c3cf64d6945ffd9b80668822a817e17ec35
lucasg1337 <lucasgprogramador2018@gmail.com>
2023-01-20 10:38:34 -0300

    att

---
+++ js_where_to.js
@@ -0,0 +1,4 @@
+function changeColor(){//função que irá ser usada no documento HTML
+    document.getElementById("btn1").style.backgroundColor = "lightgreen";//muda a cor do fundo
+    document.getElementById("btn1").style.transitionDuration = "0.8s";//em "slow motion"
+}
