88bb4a1d72d9a0cadf6ac6052ba95d69ab416b1a
lucasg1337 <lucasgprogramador2018@gmail.com>
2023-01-21 20:28:21 -0300

    my new skill

--- 1_where_to.js
+++ 1_where_to.js
@@ -2,3 +2,8 @@ function changeColor(){//função que irá ser usada no documento HTML
     document.getElementById("btn1").style.backgroundColor = "lightgreen";//muda a cor do fundo
     document.getElementById("btn1").style.transitionDuration = "0.8s";//em "slow motion"
 }
+
+//arquivos externbos podem ser referenciados de tres formas:
+//.Sem Path
+//.Endereço web
+//Com path (ex: /js/myScript.js)
